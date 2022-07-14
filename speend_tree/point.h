6bc521e992147679530cf76167a099dd3b9be275
Sanchosszza <87428097+AlexandraZgonnikova@users.noreply.github.com>
2022-07-14 14:30:49 +0300

    Update point.h

--- speend_tree/point.h
+++ speend_tree/point.h
@@ -1,1 +1,23 @@
 #pragma once
+class Point
+{
+private:
+	int id;
+	int x;
+	int y;
+	bool use;
+public:
+	Point(void);
+	Point(int id, int x, int y);
+	~Point(void);
+
+	int GetId();
+	int GetX();
+	int GetY();
+	bool GetUse();
+
+	void SetUse(bool _use);
+
+	int CalculatePathToPoint(Point* p);
+};
+
